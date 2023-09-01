056b000b153dbeff3070b9d0284b39513c198b7c
yoonmyungseok <yoonms5319@gmail.com>
2023-09-01 13:51:22 +0900

    0901

---
+++ pass-batch/src/main/generated/com/fastcampus/pass/repository/pass/PassModelMapperImpl.java
@@ -0,0 +1,31 @@
+package com.fastcampus.pass.repository.pass;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-09-01T13:29:36+0900",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.8 (Eclipse Adoptium)"
+)
+public class PassModelMapperImpl implements PassModelMapper {
+
+    @Override
+    public PassEntity toPassEntity(BulkPassEntity bulkPassEntity, String userId) {
+        if ( bulkPassEntity == null && userId == null ) {
+            return null;
+        }
+
+        PassEntity passEntity = new PassEntity();
+
+        if ( bulkPassEntity != null ) {
+            passEntity.setStatus( status( bulkPassEntity.getStatus() ) );
+            passEntity.setRemainingCount( bulkPassEntity.getCount() );
+            passEntity.setPackageSeq( bulkPassEntity.getPackageSeq() );
+            passEntity.setStartedAt( bulkPassEntity.getStartedAt() );
+            passEntity.setEndedAt( bulkPassEntity.getEndedAt() );
+        }
+        passEntity.setUserId( userId );
+
+        return passEntity;
+    }
+}
